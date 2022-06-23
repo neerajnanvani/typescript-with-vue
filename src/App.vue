9084951ade152df33a935bff48623c66278dc8be
neerajnanvani <neeraj.nanvani@vidyamantra.com>
2022-06-23 15:37:03 +0530

    buid first interface

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <template>
-  <div class="app w-full h-full text-center text-red-500 bg-gray-600">
+  <div class="app w-full h-full text-center mt-14">
     Hello typescript
+
+    <p>Hello {{name}} ({{age}})</p>
+
+    <button @click="changeName('Nanvani')" class="border border-red-400 p-3"> Change Name </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from "./types/Job";
 
 export default defineComponent({
   name: 'App',
+
+  setup() {
+    const state = reactive({
+      name: "Neeraj",
+      age: 21 as string | number
+    })
+
+    const jobs = ref<Job[]>([
+      { title: 'developer', location: 'delhi', salary: 500000, id: 1 },
+      { title: 'senior developer', location: 'noida', salary: 3000000, id: 2 },
+      { title: 'junior developer', location: 'delhi', salary: 50000, id: 3 },
+      { title: 'ui developer', location: 'up', salary: 100000, id: 4 },
+      { title: 'content writer', location: 'gujrat', salary: 300000, id: 5 },
+    ])
+
+    return { ...toRefs(state) };
+  },
+  methods: {
+
+    changeName(name: string) {
+      this.name = name;
+    }
+  }
 });
 </script>
 
---
+++ src/types/Job.ts
@@ -0,0 +1,8 @@
+ interface Job {
+    title: string,
+    location: string,
+    salary: number,
+    id: string | number
+ }
+
+ export default Job
